815c54aada315245146dddd2b386510457e60dbd
John Phillips <j.phillips@cablelabs.com>
2021-09-07 16:50:05 -0600

    Initial commit.

---
+++ transport_common.h
@@ -0,0 +1,21 @@
+#ifndef _TRANSPORT_COMMON_H_
+#define _TRANSPORT_COMMON_H_
+
+/* a "msg" is the data unit passed from layer 5 (teachers code) to layer  */
+/* 4 (students' code).  It contains the data (characters) to be delivered */
+/* to layer 5 via the students transport level protocol entities.         */
+struct msg {
+  char data[20];
+};
+
+/* a packet is the data unit passed from layer 4 (students code) to layer */
+/* 3 (teachers code).  Note the pre-defined packet structure, which all   */
+/* students must follow. */
+struct pkt {
+  int seqnum;
+  int acknum;
+  int checksum;
+  char payload[20];
+};
+
+#endif
